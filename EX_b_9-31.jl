d65e943ad4abe3c62f97cf94e985628686d9a839
pitodogo <pitodogo@gmail.com>
2016-03-08 22:50:40 +0800

    Nothing

---
+++ EX_b_9-31.jl
@@ -0,0 +1,80 @@
+using PyPlot
+function NewtonMethod(A,x,alpha,beta,tolerance)
+
+    MAXITER = 1000
+
+    history_val = []
+    
+    for i = 1:MAXITER
+    val = Value(A,x)
+    ### DIRECTION
+        grad = Gradient(A,x)
+        
+    hess = Diag_Hessian(A,x)
+        println(i)
+    x_nt = -inv(hess)*grad
+    lambda2 = grad'*inv(hess)*grad
+
+ 
+    ### LINE SEARCH        
+    ### PRACITICALLY, A FEASIBLE t must be bound first before line search
+        t = 1
+        while((maximum(A*(x+t * x_nt)) >= 1 || maximum(abs(x + t * x_nt)) >= 1))
+            t = beta * t
+        end
+
+        while( Value(A,x + t * x_nt) > val - alpha * t * ((x_nt'*x_nt)[1]))
+            t = beta * t
+        end
+
+    ## UPDATE
+            x = x + t * x_nt
+
+    push!(history_val,Value(A,x))
+
+    ### CHECK EXIT CRITERIA
+        if(abs(lambda2[1]/2)  <= tolerance)
+            return x,val,history_val
+        end                
+
+    end
+
+    return x,val,history_val
+end
+
+
+function Value(A,x)    
+    return -1*( sum(log(1-A*x)) + sum(log(1+x)) + sum(log(1-x)))
+end
+
+function Gradient(A,x)
+   return A'*(1./(1-A*x)) - 1./(1+x) + 1./(1-x)
+end
+
+function Diag_Hessian(A,x)
+    d = 1./(1-A*x)
+    p = A'*d
+    d = diag(p*p')
+    m = size(A)[1]
+    n = size(A)[2]
+
+    return -1*Diagonal(d) + Diagonal(reshape(1./(1+x).^2 + 1./(1-x).^2,n,))
+end
+            
+function main()
+
+    n = 100
+    m = 200
+    x = zeros(n,1)
+    A = rand(m,n)
+    alpha = 0.01
+    beta = 0.5
+    tolerance = 0.001
+    answer,value,history = NewtonMethod(A,x,alpha,beta,tolerance)
+
+    x_axis = 1:length(history)
+               plot(x_axis,history-value)
+               show()
+end
+
+main()
