201267c7ed664c0f39fd11ab08e30dad4d2381d5
pitodogo <rootlesstree2@gmail.com>
2016-03-14 23:22:52 +0800

    Finish EX10-15

--- CH10/EX10-15b.jl
+++ CH10/EX10-15b.jl
@@ -1,19 +1,36 @@
-include("EX10-15a.jl")
-
+include("parameter_generator.jl")
 
 for iteration = 1:1000
+println(iteration)
 grad = Grad(x)
 hess = Hess(x)
 
-    AA = [hess A';A 0*eye(p)]
-    bb = [-grad();Ax-b]
-    s = AA\bb
-    x_nt = s[1:n]
-    v_nt = s[n+1:end]
-    t = 1
-    while(minimum(x+t*x_nt) <= 0)
-        t = beta*t;
+AA = [hess A';A 0*eye(p)]
+
+
+
+bb = -1*[Grad(x);A*x-b]
+s = AA\bb
+x_nt = s[1:n]
+v_nt = s[n+1:end] - v
+r = [Grad(x) + A'*v;A*x-b]    
+
+
+t = 1
+while(minimum(x+t*x_nt) <= 0)
+     t = beta*t;
+end
+
+    #### LINE SEARCH
+    
+    while(norm([Grad(x+t*x_nt) + A'*(v+t*v_nt);A*(x + t*x_nt)-b]) > (1-alpha*t)*norm(r))
+        t = beta*t
     end
 
-        while(
+        x = x + t * x_nt
+        v = v + t * v_nt
+        if (norm(A*x - b) < tolerance && norm(r) <= tolerance)
+            break
+        end
+        
 end
